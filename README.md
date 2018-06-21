2a8ec7ed7e793d7f16024566219e84e18480c60b
Danurai <Danurai@users.noreply.github.com>
2018-06-21 17:20:54 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
 # xwingdata
 
-A Clojure library designed to ... well, that part is up to you.
+View Xwing 1.0 upgrade cards
+Card data requested using taoensso/sente over websocket, sente runs a keepalive ping
 
-## Usage
+## Overview
 
-FIXME
+FIXME: Write a paragraph about the library/project and highlight its goals.
+
+## Setup
+
+Set up a repl, run the server and start figwheel monitoring
+
+    lein repl
+    (ns user)
+    (reset)
+    (fig-start)
+    
+open your browser at [localhost:9009](http://localhost:9009/).
+
+To clean all compiled files:
+
+    lein clean
+
+To create a production build run:
+
+    lein do clean, cljsbuild once min
+
+And open your browser in `resources/public/index.html`. You will not
+get live reloading, nor a REPL. 
 
 ## License
 
-Copyright © 2018 FIXME
+Copyright © 2014 FIXME
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
--- project.clj
+++ project.clj
@@ -1,53 +1,119 @@
 (defproject xwingdata "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-   :description   "An XWing Data Viewer written with Clojure and Clojurescript."
-   :url "https://danurai.github.io/xwingdata"
-   :license {:name "GNU General Public License"
-            :url "http://www.gnu.org/licenses/gpl.html"}
-   
-   :min-lein-version "2.7.0"
-   
-   :main xwingdata.system
-   
-   :jar-name     "xwingdata.jar"
-   :uberjar-name "xwingdata-standalone.jar"
-   
-   :dependencies [[org.clojure/clojure "1.8.0"]
-                [org.clojure/clojurescript "1.9.946"]
-                [environ "1.1.0"]
-                [http-kit "2.2.0"]
-                [com.stuartsierra/component "0.3.2"]
-                [compojure "1.6.0"]
-                [reagent "0.7.0"]
-                [jarohen/chord "0.8.1"]
-                [org.clojure/core.async "0.3.465"]]
-                
-   :figwheel {:css-dirs ["resources/public/css"]} ;; watch and update CSS    
-   
-   :profiles {:uberjar {:aot :all
+  :main xwingdata.system
+
+  :jar-name     "xwingdata.jar"
+  :uberjar-name "xwingdata-standalone.jar"
+  
+  :min-lein-version "2.7.1"
+
+  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
+               [org.clojure/clojurescript "1.9.946"]
+               [org.clojure/core.async  "0.3.443"]
+              ; Web server
+               [http-kit "2.2.0"]
+               [com.stuartsierra/component "0.3.2"]
+              ; routing
+               [compojure "1.6.0"]
+               [ring/ring-defaults "0.3.1"]
+              ; Websocket sente
+               [com.taoensso/sente "1.12.0"]
+              ; page rendering
+               [hiccup "1.0.5"]
+               [reagent "0.7.0"]]
+
+  :plugins [[lein-figwheel "0.5.14"]
+           [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
+           [lein-autoexpect "1.9.0"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+
+                ;; The presence of a :figwheel configuration here
+                ;; will cause figwheel to inject the figwheel client
+                ;; into your build
+                :figwheel true
+                :compiler {:main xwingdata.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/xwingdata.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true
+                           ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
+                           ;; https://github.com/binaryage/cljs-devtools
+                           :preloads [devtools.preload]}}
+               ;; This next build is a compressed minified build for
+               ;; production. You can build this with:
+               ;; lein cljsbuild once min
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/xwingdata.js"
+                           :main xwingdata.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
+             ;; :server-port 3449 ;; default
+             ;; :server-ip "127.0.0.1"
+
+             :css-dirs ["resources/public/css"] ;; watch and update CSS
+
+             ;; Start an nREPL server into the running figwheel process
+             ;; :nrepl-port 7888
+
+             ;; Server Ring Handler (optional)
+             ;; if you want to embed a ring handler into the figwheel http-kit
+             ;; server, this is for simple ring servers, if this
+
+             ;; doesn't work for you just run your own server :) (see lein-ring)
+
+             ;; :ring-handler hello_world.server/handler
+
+             ;; To be able to open files in your editor from the heads up display
+             ;; you will need to put a script on your path.
+             ;; that script will have to take a file path and a line number
+             ;; ie. in  ~/bin/myfile-opener
+             ;; #! /bin/sh
+             ;; emacsclient -n +$2 $1
+             ;;
+             ;; :open-file-command "myfile-opener"
+
+             ;; if you are using emacsclient you can just use
+             ;; :open-file-command "emacsclient"
+
+             ;; if you want to disable the REPL
+             ;; :repl false
+
+             ;; to configure a different figwheel logfile path
+             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
+
+             ;; to pipe all the output to the repl
+             ;; :server-logfile false
+             }
+
+
+  ;; Setting up nREPL for Figwheel and ClojureScript dev
+  ;; Please see:
+  ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
+  :profiles {:uberjar {:aot :all
                       :source-paths ["src"]
                       :prep-tasks ["compile" ["cljsbuild" "once" "min"]]}
-             :dev    {:plugins [[lein-cljsbuild "1.1.7"]
-                              [lein-figwheel "0.5.14"]
-                              [lein-autoexpect "1.9.0"]]
-                      :dependencies [[reloaded.repl "0.2.4"]
-                                   [expectations "2.2.0-rc3"]]
-                      :source-paths ["dev"]}}
-                   
-   :cljsbuild {:builds [{:id "dev"
-                       :source-paths ["dev" "src"]
-                       :figwheel true
-                       :compiler {:output-to "target/classes/public/xwingapp.js"
-                                 :output-dir "target/classes/public/out"
-                                 :main "xwingdata.client"
-                                 :asset-path "/out"
-                                 :optimizations :none
-                                 :recompile-dependents true
-                                 :source-map true}}
-                      {:id "min"
-                       :source-paths ["src"]
-                       :compiler {:output-to "resources/public/xwingapp.js"
-                                 :main "xwingdata.client"
-                                 :closure-defines {xwingdata.client/ws-uri "wss://xwingdata.herokuapp.com/ws"}
-                                 :optimizations :advanced
-                                 :pretty-print false}}]})
+            :dev {:dependencies [[binaryage/devtools "0.9.4"]
+                              [figwheel-sidecar "0.5.14"]
+                              [com.cemerick/piggieback "0.2.2"]
+                              [reloaded.repl "0.2.4"]
+                              [expectations "2.2.0-rc3"]]
+                   ;; need to add dev source path here to get user.clj loaded
+                   :source-paths ["src" "dev"]
+                   ;; for CIDER
+                   ;; :plugins [[cider/cider-nrepl "0.12.0"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                   ;; need to add the compliled assets to the :clean-targets
+                   :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                                     :target-path]}})
